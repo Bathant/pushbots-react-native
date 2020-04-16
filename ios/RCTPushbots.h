14918f7ef475d5e9209c0ee3548e5b8c223f8de1
bathant hegazy <bathanthegazy94@gmail.com>
2020-04-16 20:56:16 +0200

    modify framework & .h files

--- ios/RCTPushbots.h
+++ ios/RCTPushbots.h
@@ -11,6 +11,9 @@
 #import "Pushbots.h"
 #endif
 #import <React/RCTEventEmitter.h>
+#import <WebKit/WebKit.h>
+@import CoreTelephony;
+@import WebKit;
 @interface RCTPushbots : RCTEventEmitter <RCTBridgeModule>
 - (id) initWithAppId:(NSString*)appId withLaunchOptions:(NSDictionary *)launchOptions;
 - (id) initWithAppId:(NSString*)appId withLaunchOptions:(NSDictionary *)launchOptions prompt:(BOOL)prompt;
